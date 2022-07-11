70244775f6cc120a782a566bb56d47d15e5ae70b
PritamNandy <prasun.nandy@northsouth.edu>
2022-07-12 00:25:13 +0600

    change footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="footer d-flex flex-column flex-md-row align-items-center justify-content-between px-4 py-3 border-top small">
-    <p class="text-muted mb-1 mb-md-0">Copyright © 2022 <a href="https://www.nobleui.com" target="_blank">Hyperlook</a>.</p>
+    <p class="text-muted mb-1 mb-md-0">Copyright © 2022 <a href="#" target="_blank">copulare</a>.</p>
     <p class="text-muted">All rights reserved <i class="mb-1 text-primary ms-1 icon-sm" data-feather="heart"></i></p>
 </footer>
 
